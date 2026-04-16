a79fd5496d16abde75f5d8e30dee8023cfe91aec
Andre Destro <andre.destro@outsystems.com>
2026-04-16 17:13:11 +0100

    feat: add cordova-zsh-plugin formula

---
+++ Formula/cordova-zsh-plugin.rb
@@ -0,0 +1,32 @@
+class CordovaZshPlugin < Formula
+  desc "Zsh plugin providing aliases and tab completion for Apache Cordova"
+  homepage "https://github.com/andredestro/cordova-zsh-plugin"
+  url "https://github.com/andredestro/cordova-zsh-plugin/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "38c5bde0a94c9ac517ed7d0a75636468b90776a309b3abce1a6dc7030f639a34"
+  version "1.0.0"
+  license "MIT"
+
+  def install
+    (share/"cordova-zsh-plugin").install "cordova.plugin.zsh"
+    (share/"zsh/site-functions").install "_cordova"
+  end
+
+  def caveats
+    <<~EOS
+      To activate the plugin, add the following to your ~/.zshrc:
+
+        source "#{share}/cordova-zsh-plugin/cordova.plugin.zsh"
+
+      Tab completion is installed to:
+        #{share}/zsh/site-functions/_cordova
+
+      If completion is not working, rebuild the zsh compdump cache:
+        rm -f ~/.zcompdump && exec zsh
+    EOS
+  end
+
+  test do
+    assert_path_exists share/"cordova-zsh-plugin/cordova.plugin.zsh"
+    assert_path_exists share/"zsh/site-functions/_cordova"
+  end
+end
