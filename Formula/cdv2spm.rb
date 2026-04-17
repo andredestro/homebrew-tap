91277597779f2209fedbfa8cc366b5b25d943fdf
Andre Destro <andre.destro@outsystems.com>
2026-04-17 16:50:07 +0100

    chore: bump cdv2spm to v1.0.1

--- Formula/cdv2spm.rb
+++ Formula/cdv2spm.rb
@@ -1,9 +1,9 @@
 class Cdv2spm < Formula
   desc "Converts Cordova plugin.xml to Swift Package Manager Package.swift"
   homepage "https://github.com/andredestro/cordova-plugin-converter"
-  url "https://github.com/andredestro/cordova-plugin-converter/releases/download/v1.0.0/cdv2spm-v1.0.0-macos.zip"
-  sha256 "8209a917e18fd58bbe638f989d1f95e7aac381399d2e6078dc5719f4a9bba426"
-  version "1.0.0"
+  url "https://github.com/andredestro/cordova-plugin-converter/releases/download/v1.0.1/cdv2spm-v1.0.1-macos.zip"
+  sha256 "d60da2113261387e354df47476bb62f2ac496c6d653a71c318f33d1403bf514b"
+  version "1.0.1"
   license "MIT"
 
   def install
